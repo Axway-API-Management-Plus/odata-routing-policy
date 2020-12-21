31f2baff29a77877f5ea81d231c04059bb09a942
Chris Wiechmann <cwiechmann@axway.com>
2020-12-21 19:42:00 +0100

    Update ODataV3Tests.cs

--- converter/ODataConverter.Tests/odatav3tests/ODataV3Tests.cs
+++ converter/ODataConverter.Tests/odatav3tests/ODataV3Tests.cs
@@ -29,8 +29,8 @@ namespace ODataConverterV3.Tests
         {
             string pathAssembly = System.Reflection.Assembly.GetExecutingAssembly().Location;
             string folderAssembly = System.IO.Path.GetDirectoryName(pathAssembly);
-            if (folderAssembly.EndsWith("\\") == false) folderAssembly = folderAssembly + "\\";
-            string folderProjectLevel = System.IO.Path.GetFullPath(folderAssembly + "..\\..\\..\\");
+            if (folderAssembly.EndsWith("//") == false) folderAssembly = folderAssembly + "//";
+            string folderProjectLevel = System.IO.Path.GetFullPath(folderAssembly + "..//..//..//");
             return folderProjectLevel;
         }
     }
