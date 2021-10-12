242fec5a7bfaff01679f06fbaee6a8942d2cb90b
rathna <rathnapandi.n@gmail.com>
2021-10-12 12:20:43 -0700

    Fix #15

---
+++ converter/odata3/HttpResponseValue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdataSwaggerConverter
+{
+    class HttpResponseValue
+    {
+        private string responseBody;
+        private string contentType;
+        private int statusCode;
+        public HttpResponseValue(string responseBody, string contentType, int statusCode)
+        {
+            this.responseBody = responseBody;
+            this.contentType = contentType;
+            this.statusCode = statusCode;
+        }
+
+        public string getResponseBody()
+        {
+            return responseBody;
+        }
+
+        public string getContentType()
+        {
+            return contentType;
+        }
+
+        public int getStatusCode()
+        {
+            return statusCode;
+        }
+
+
+    }
+}
