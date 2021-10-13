a762277e63b7d020d5e1724a57a1b9abc5667f3d
Rathna <rathnapandi.n@gmail.com>
2021-10-12 17:27:28 -0700

    Update Build.cmd

--- .github/workflows/Build.cmd
+++ .github/workflows/Build.cmd
@@ -1,1 +1,8 @@
-
+@echo off
+pushd "%~dp0"
+if exist Debug rd /s /q Debug
+if exist Release rd /s /q Release
+"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release /p:Platform=x86
+:exit
+popd
+@echo on
